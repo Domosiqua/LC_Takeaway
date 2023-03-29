665b771d5b949ef33f9da7b6e3a32d8ab0d72b23
Domosiqua <2961777212@qq.com>
2023-03-29 16:39:37 +0800

    6

--- src/main/java/com/LctakeawayApplication.java
+++ src/main/java/com/LctakeawayApplication.java
@@ -1,13 +1,16 @@
 package com;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class LctakeawayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LctakeawayApplication.class, args);
+        log.info("wossb");
     }
 
 }
